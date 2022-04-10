dfcbf48469d77fe5a78a68bf765350e062055b31
kansei <kansei@kansei.local>
2022-04-10 14:34:12 +0900

    data append

---
+++ popular.py
@@ -0,0 +1,66 @@
+import os
+from tqdm import tqdm
+
+import sekitoba_library as lib
+import sekitoba_data_manage as dm
+
+dm.dl.file_set( "race_data.pickle" )
+dm.dl.file_set( "race_info_data.pickle" )
+dm.dl.file_set( "horce_data_storage.pickle" )
+
+def main():
+    result = {}
+    race_data = dm.dl.data_get( "race_data.pickle" )
+    race_info = dm.dl.data_get( "race_info_data.pickle" )
+    horce_data = dm.dl.data_get( "horce_data_storage.pickle" )
+    key_dict = {}
+    
+    for k in tqdm( race_data.keys() ):
+        race_id = lib.id_get( k )
+        year = race_id[0:4]
+        race_place_num = race_id[4:6]
+        day = race_id[9]
+        num = race_id[7]
+
+        key_place = str( race_info[race_id]["place"] )
+        key_dist = str( race_info[race_id]["dist"] )
+        key_kind = str( race_info[race_id]["kind"] )        
+        key_baba = str( race_info[race_id]["baba"] )
+
+        if year in lib.test_years:
+            continue
+
+        #芝かダートのみ
+        if key_kind == "0" or key_kind == "3":
+            continue
+
+        for kk in race_data[k].keys():
+            horce_id = kk
+            current_data, past_data = lib.race_check( horce_data[horce_id],
+                                                     year, day, num, race_place_num )#今回と過去のデータに分ける
+            cd = lib.current_data( current_data )
+            pd = lib.past_data( past_data, current_data )
+
+            if not cd.race_check():
+                continue
+
+            key = str( int( cd.popular() ) )
+            lib.dic_append( result, year, {} )
+            lib.dic_append( result[year], key, { "recovery": 0, "count": 0 } )
+
+            result[year][key]["count"] += 1
+
+            if cd.rank() == 1:
+                result[year][key]["recovery"] += cd.odds()
+
+
+    for year in result.keys():
+        for k in result[year].keys():
+            result[year][k]["recovery"] /= result[year][k]["count"]
+            result[year][k]["recovery"] = round( result[year][k]["recovery"], 2 )
+
+    lib.write_recovery_csv( result, "popular.csv" )
+
+if __name__ == "__main__":
+    main()
+        
