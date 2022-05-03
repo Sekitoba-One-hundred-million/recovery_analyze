a8768d54e29dd30020c1806c48daf698a18121cf
kansei <kansei@kansei.local>
2022-05-03 17:15:20 +0900

    select update data

--- main.sh
+++ main.sh
@@ -3,32 +3,45 @@ file_list=`ls analyze`
 write_file_name="$dir/name.py"
 list_file="$dir/list.txt"
 
-rm -rf $dir
-mkdir $dir
-echo 'class Name:' >> $write_file_name
-echo '    def __init__( self ):' >> $write_file_name
+echo "1: all update"
+echo "2: name update"
+echo "3: recovery_score update"
+echo "4: users_score update"
 
-for file_name in $file_list; do
-    base='        self.'
-    ARR=(${file_name//./ })
-    name=${ARR[0]}
-    # echo $file_name
-    echo "$base$name = \"$name\"" >> $write_file_name
-    echo $name >> $list_file
-done
+tag=""
+read -p "Enter 1,2,3,4 > " tag
 
-re_dir="recovery_score"
-us_dir="users_score"
-
-cp -r $dir $re_dir/
-cp -r $dir $us_dir/
-
-if [ $# -gt 0 ]; then
-    echo $1
-    if [ $1 = "-n" ]; then
-       exit 1
-    fi
+if [ ! $tag = "1" ] && [ ! $tag = "2" ] && [ ! $tag = "3" ] && [ ! $tag = "4" ]; then
+    echo "Wrong number"
+    #echo "Enter 1,2,3,4"
 fi
 
-python $re_dir/main.py
-python $us_dir/main.py
+if [ $tag = "1" ] || [ $tag = "2" ]; then 
+    rm -rf $dir
+    mkdir $dir
+    echo 'class Name:' >> $write_file_name
+    echo '    def __init__( self ):' >> $write_file_name
+    
+    for file_name in $file_list; do
+        base='        self.'
+        ARR=(${file_name//./ })
+        name=${ARR[0]}
+        # echo $file_name
+        echo "$base$name = \"$name\"" >> $write_file_name
+        echo $name >> $list_file
+    done
+    
+    re_dir="recovery_score"
+    us_dir="users_score"
+    
+    cp -r $dir $re_dir/
+    cp -r $dir $us_dir/
+fi
+
+if [ $tag = "1" ] || [ $tag = "3" ]; then
+    python $re_dir/main.py
+fi
+
+if [ $tag = "1" ] || [ $tag = "4" ]; then
+    python $us_dir/main.py
+fi
